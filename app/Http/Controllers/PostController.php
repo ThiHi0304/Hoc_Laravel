cc6dc48ea84def0cd1337418a461b2e48ca508fe
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-04-01 22:44:19 +0700

    Completed lesson 45

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 class PostController extends Controller
 {
     //
@@ -12,11 +13,48 @@ class PostController extends Controller
         // dd($allProducts);
         // $post=Post::find('c1');
         // dd($post);
-        $post=new Post;
-        $post->title='Bài viết 3';
-        $post->content='Nội dung 3';
+        // $post=new Post;
+        // $post->title='Bài viết 3';
+        // $post->content='Nội dung 3';
         // $post->status=1;
-        $post->save();
+        // $post->save();
+        echo '<h2>Query Eloquent Model</h2>';
+        $allPost=Post::all();
+        // dd($allPost);
+        // if($allPost->count()>0){
+        //     foreach($allPost as $item){
+        //         echo $item->title.'<br/>';
+        //     }
+        // }
+        // $detail=Post::find(1);
+        // dd($detail);
+        // $activePosts=DB::table('posts')->where('status',1)->get();
+        // $activePosts=Post::where('status',1)->orderBy('id','DESC')->get();
+        // if($activePosts->count()>0){
+        //     foreach($activePosts as $item){
+        //         echo $item->title.'<br/>';
+        //     }
+        // }
+        // dd($activePosts);
+        // $allPosts=Post::all();
+        // $activePosts=$allPost->reject(function($post){
+        //     // dd($post);
+        //     return $post->status==0;
+        // });
+        // dd($activePosts);
+        // Post::chunk(2,function ($post){
+        //     foreach($post as $post){
+        //     echo $post->title.'<br/>';
+        //     }
+        //     echo 'Kết thúc'.'<br/>';
+        // });
+        //  $allPosts=Post::where('status',1)->cursor();
+        //  dd($allPost);
+        $allPosts=Post::cursor();
+        foreach($allPosts as $item){
+               echo $item->title.'<br/>';
+               }
+        dd($allPosts);
     }
 
 }
